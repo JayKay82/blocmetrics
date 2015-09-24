32eace9c6cbbb465f74fe4032bb6f9f69f0aa284
Jan Kroezen <jankroezen@me.com>
2015-09-24 17:35:21 +0100

    Complete checkpoints 2 & 3, Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create a user
+user = User.new(
+  name: 'Jan',
+  email: 'jankroezen@me.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+user.skip_confirmation!
+user.save!
+
+puts "#{User.count} users created."
